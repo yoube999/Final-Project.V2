1efa76da0ce666ea12aa45af00934151b78ffb8e
LAPTOP-U1BI9D26\to60412 <to60412@gmail.com>
2023-09-21 13:52:55 +0800

    測試stash

--- tw.com.eeit168/src/main/java/tw/com/eeit168/service/TestClass.java
+++ tw.com.eeit168/src/main/java/tw/com/eeit168/service/TestClass.java
@@ -3,8 +3,8 @@ package tw.com.eeit168.service;
 public class TestClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO 
+		System.out.println("456");
 	}
 
 }
