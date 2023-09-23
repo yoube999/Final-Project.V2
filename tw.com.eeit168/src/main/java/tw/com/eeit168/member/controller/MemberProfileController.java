a118bbce9581fc4996da5016554a6dc931c4438f
LAPTOP-U1BI9D26\to60412 <to60412@gmail.com>
2023-09-24 01:08:02 +0800

    Controller register 完成，但postman測試異常

--- tw.com.eeit168/src/main/java/tw/com/eeit168/member/controller/MemberProfileController.java
+++ tw.com.eeit168/src/main/java/tw/com/eeit168/member/controller/MemberProfileController.java
@@ -1,7 +1,10 @@
 package tw.com.eeit168.member.controller;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,31 +12,40 @@ import org.springframework.web.bind.annotation.RestController;
 
 import tw.com.eeit168.member.service.MemberProfileService;
 
-
 @RestController
 @RequestMapping("/eeit168/member")
 
 public class MemberProfileController {
-	
+
 	@Autowired
 	private MemberProfileService memberProfileService;
-	
+
 	@Autowired
 	private MessageSource messageSource;
-	
-	
+
 	// 前端Post送字串
 	@PostMapping("/login")
 	public String login(@RequestParam String user_account, String user_password) {
-		
-	boolean success = memberProfileService.login(user_account, user_password);
-	if(success) {
+
+		boolean success = memberProfileService.login(user_account, user_password);
+		if (success) {
 			return "登入成功";
-	} else {
-		return "登入失敗";
+		} else {
+			return "登入失敗";
+		}
+
 	}
-		
+
+	@PostMapping("/register")
+	public String register(@RequestParam String user_account,
+			@RequestParam String user_password,
+			@RequestParam String username, 
+			@RequestParam String gender, 
+			@RequestParam String phone_number,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date birthday) {
+
+		memberProfileService.registerMember(user_account, user_password, username, gender, phone_number, birthday);
+
+		return "註冊成功";
 	}
-	
-	
 }
--- tw.com.eeit168/src/main/java/tw/com/eeit168/member/service/MemberProfileService.java
+++ tw.com.eeit168/src/main/java/tw/com/eeit168/member/service/MemberProfileService.java
@@ -1,11 +1,11 @@
 package tw.com.eeit168.member.service;
 
-import java.text.SimpleDateFormat;
-import java.text.ParseException;
+import java.util.Date; // 导入日期类
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import java.util.Date; // 导入日期类
+
 import tw.com.eeit168.member.dao.MemberProfileInterFace;
 import tw.com.eeit168.member.model.MemberProfileBean;
 
