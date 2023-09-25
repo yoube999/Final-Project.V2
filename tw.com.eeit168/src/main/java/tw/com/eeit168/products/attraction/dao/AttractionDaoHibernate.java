b710122b649847511a958703744d70d26ba122c1
Tseng <spalding0930@gmail.com>
2023-09-25 10:23:41 +0800

    重寫attraction dao

--- tw.com.eeit168/src/main/java/tw/com/eeit168/products/attraction/dao/AttractionDaoHibernate.java
+++ tw.com.eeit168/src/main/java/tw/com/eeit168/products/attraction/dao/AttractionDaoHibernate.java
@@ -1,5 +1,7 @@
 package tw.com.eeit168.products.attraction.dao;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 
@@ -21,4 +23,43 @@ public class AttractionDaoHibernate implements AttractionDAO{
 		return this.getSession().get(AttractionBean.class, id);
 	}
 	
+	@Override
+	public List<AttractionBean> selectAll() {
+		return this.getSession().createQuery("from AttractionBean", AttractionBean.class).list();
+	}
+	
+	@Override
+	public AttractionBean insert(AttractionBean bean) {
+		if(bean != null && bean.getAttractions_id() != null) {
+			AttractionBean temp = this.getSession().get(AttractionBean.class, bean.getAttractions_id());
+			if(temp == null) {
+				this.getSession().persist(bean);
+				return bean;
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public AttractionBean update(AttractionBean bean) {
+		if(bean != null && bean.getAttractions_id() != null) {
+			AttractionBean temp = this.getSession().get(AttractionBean.class, bean.getAttractions_id());
+			if(temp != null) {
+				return (AttractionBean)this.getSession().merge(bean);
+			}
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean delete(Integer id) {
+		if(id != null) {
+			AttractionBean temp = this.getSession().get(AttractionBean.class, id);
+			if(temp != null) {
+				this.getSession().remove(temp);
+				return true;
+			}
+		}
+		return false;
+	}
 }
