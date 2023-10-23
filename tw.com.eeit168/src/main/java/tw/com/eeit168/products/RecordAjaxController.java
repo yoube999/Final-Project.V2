62c623a9cc213c0fcc65ad25ff9d489994051726
LAPTOP-U1BI9D26\to60412 <to60412@gmail.com>
2023-10-23 16:53:37 +0800

    更改後端的推貨

--- tw.com.eeit168/src/main/java/tw/com/eeit168/products/RecordAjaxController.java
+++ tw.com.eeit168/src/main/java/tw/com/eeit168/products/RecordAjaxController.java
@@ -1,7 +1,12 @@
 package tw.com.eeit168.products;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+//import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -101,8 +106,8 @@ public class RecordAjaxController {
 						.put("record_status", record.getRecordStatus())
 						.put("return_title", record.getReturnTitle())
 						.put("return_description", record.getReturnDescription())
-						.put("return_date", record.getReturnDate());
-
+						.put("return_date", record.getReturnDate())
+						.put("total_price", record.getTotalPrice());
 				array = array.put(item);			
 			}
 		}
@@ -123,14 +128,26 @@ public class RecordAjaxController {
 	            // 更新订单记录的退货信息
 	            existingRecord.setReturnTitle(updateData.get("return_title"));
 	            existingRecord.setReturnDescription(updateData.get("return_description"));
-	            
+	            existingRecord.setRecordStatus(updateData.get("record_status"));
+
 	            // 设置当前日期
 	            // 设置当前日期
-	            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	            Date currentDate = new Date();
-	            
-	            // 调用Service更新订单记录
+	            LocalDateTime now = LocalDateTime.now();
+//	            LocalDate now = LocalDate.now();
+//	            LocalTime time = LocalTime.now();  // 设置时分秒为 00:00:00
+//	            LocalDateTime dateTime = now.atTime(time);
+	            // 格式化为日期字符串
+	            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+	            String formattedDateTime = now.format(formatter);
+	            // 将时间字符串转换为 Timestamp
+	            Timestamp timestamp = Timestamp.valueOf(formattedDateTime);       
+	            // 将 Timestamp 转换为 Date
+	            Date date = new Date(timestamp.getTime());
+	            // 设置退货时间
+	            existingRecord.setReturnDate(date);
+	            // 调用 Service 更新订单记录
 	            recordRepositorySerivce.updateRecord(existingRecord);
+
 	            
 	            message = "订单记录更新成功";
 	            status = HttpStatus.OK;
